c2d9d0cb8b00f089dc767cc8c27ce059442e21b1
Bogdan Mitroiu <mitroiu.bogdan@gmail.com>
2017-10-05 12:36:04 +0300

    Updated class Message

--- src/seriaf/oop/project/Message.java
+++ src/seriaf/oop/project/Message.java
@@ -3,5 +3,14 @@ package seriaf.oop.project;
 
 
 public class Message {
+    private String mSender;
+    private String mContent;
+    
+    public Message(String mSender,String mContent){
+        this.mSender=mSender;
+        this.mContent=mContent;
+    }
+    public String toString(){
+        return this.mSender+": "+this.mContent;}
     
 }
