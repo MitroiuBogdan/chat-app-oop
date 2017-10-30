3e9a2fdf2814179e462896e94cc6f2a698b231b4
Yllu <mitroiu.bogdan@gmail.com>
2017-10-31 01:57:23 +0200

    Create PrivateMessage.java

---
+++ src/seriaf/oop/project/PrivateMessage.java
@@ -0,0 +1,16 @@
+public class PrivateMessage extends Message{
+       private String mRecipient;
+         
+ public PrivateMessage(String mRecipent,String mSender,String mContent){
+      super(mSender,mContent);
+        this.mRecipient=mRecipent;
+        }
+@Override        
+ public String toString(){
+         return"(priv) "+super.toString();
+}
+ public String getRecipient(){
+      return  this.mRecipient;
+ }
+ }    
+     
